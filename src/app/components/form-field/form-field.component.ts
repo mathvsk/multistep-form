6eb0b33325b838ba66db4682caff76227ebc80c9
Matheus Viscki <matheusviscki@gmail.com>
2025-04-19 13:39:20 -0300

    feat: criar componente de input

---
+++ src/app/components/form-field/form-field.component.ts
@@ -0,0 +1,16 @@
+import { Component, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  imports: [],
+  templateUrl: './form-field.component.html',
+  styleUrl: './form-field.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'input-component',
+  }
+})
+export class FormFieldComponent {
+  title = input.required<string>();
+  showError = input(false);
+}
